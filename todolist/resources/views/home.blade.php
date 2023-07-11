9d52a3307cc56dc76b2155fb1042a8cb2202049a
tiago.augusto <tiagoxistech@gmail.com>
2023-07-11 14:28:53 -0300

    first commit

---
+++ todolist/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Projeto Todo-List</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Todo-List">
+    <meta name="keywords" content="html, css, js, site">
+    <meta name="robots" content="home">
+</head>
+<body>
+
+</body>
+</html>
